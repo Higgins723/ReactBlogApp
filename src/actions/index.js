9895d97ee016f6a192333885cb3838095da6a1da
Joshua-Higgins <joshua.higgins@rakuten.com>
2018-03-21 16:24:54 -0600

    rendering a list of posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=josh12344321';
+const API_KEY = '?key=josh1234';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
